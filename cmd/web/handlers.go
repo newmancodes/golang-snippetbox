edc251d9ebe5624417a399fdb3c8a371e0616bb9
Stephen Newman <steve@newman.digital>
2025-10-23 22:40:46 +0100

    Refactor code to a better structure.

---
+++ cmd/web/handlers.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"strconv"
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Server", "Go")
+	io.WriteString(w, "Hello from Snippetbox")
+}
+
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(r.PathValue("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+
+	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
+}
+
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "Display a form for creating a new snippet...")
+}
+
+func snippetCreatePost(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusCreated)
+	io.WriteString(w, "Safe a new snippet...")
+}
