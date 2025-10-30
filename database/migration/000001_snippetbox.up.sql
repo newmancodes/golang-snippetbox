04295154f684426daed09e6a3f3f39b3dd7a34a3
Stephen Newman <steve@newman.digital>
2025-10-30 09:01:11 +0000

    Fix inserts so newlines are present.

--- database/migration/000001_snippetbox.up.sql
+++ database/migration/000001_snippetbox.up.sql
@@ -13,21 +13,33 @@ CREATE INDEX idx_snippets_created ON snippets(created);
 -- Add some dummy records (which we'll use in the next couple of chapters).
 INSERT INTO snippets (title, content, created, expires) VALUES (
     'An old silent pond',
-    'An old silent pond...\nA frog jumps into the pond,\nsplash! Silence again.\n\n- Matsuo Bashō',
+    'An old silent pond...
+A frog jumps into the pond,
+splash! Silence again.
+
+- Matsuo Bashō',
     now() AT TIME ZONE 'UTC',
     now() AT TIME ZONE 'UTC' + INTERVAL '1 year'
 );
 
 INSERT INTO snippets (title, content, created, expires) VALUES (
     'Over the wintry forest',
-    'Over the wintry\nforest, winds howl in rage\nwith no leaves to blow.\n\n- Natsume Soseki',
+    'Over the wintry
+forest, winds howl in rage
+with no leaves to blow.
+
+- Natsume Soseki',
     now() AT TIME ZONE 'UTC',
     now() AT TIME ZONE 'UTC' + INTERVAL '1 year'
 );
 
 INSERT INTO snippets (title, content, created, expires) VALUES (
     'First autumn morning',
-    'First autumn morning\nthe mirror I stare into\nshows my father''s face.\n\n- Murakami Kijo',
+    'First autumn morning
+the mirror I stare into
+shows my father''s face.
+
+- Murakami Kijo',
     now() AT TIME ZONE 'UTC',
     now() AT TIME ZONE 'UTC' + INTERVAL '1 year'
 );
